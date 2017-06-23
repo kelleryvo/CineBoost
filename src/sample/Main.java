616e2e62f2fcfdd4410c7aedc76cd40e252b6a97
yvokeller <keller.yvo@gmail,com>
2017-06-23 09:06:33 +0200

    changes in fxml, moved files

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader .load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader .load(getClass().getResource("design/main.fxml"));
+        primaryStage.setTitle("CineBoost");
         primaryStage.setScene(new Scene(root, 1080, 720));
         primaryStage.show();
     }
@@ -21,4 +21,3 @@ public class Main extends Application {
         launch(args);
     }
 }
-// Hello88
