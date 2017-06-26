97894af75e0498247987f2ce9adb27fadda3fe5f
Dennis Schappi <dennisschaeppi@outlook.com>
2017-06-26 15:03:27 +0200

    -

---
+++ src/sample/object/Angebot.java
@@ -0,0 +1,73 @@
+package sample.object;
+
+/**
+ * Created by denni on 26.06.2017.
+ */
+public class Angebot {
+    private String datum;
+    private String zeit;
+    private String film;
+    private String sprache;
+    private String dimension;
+    private String saal;
+
+    public void Create(){
+
+    }
+
+    public void Save(){
+
+    }
+
+    public void Delete(){
+
+    }
+
+    public String getDatum() {
+        return datum;
+    }
+
+    public void setDatum(String datum) {
+        this.datum = datum;
+    }
+
+    public String getZeit() {
+        return zeit;
+    }
+
+    public void setZeit(String zeit) {
+        this.zeit = zeit;
+    }
+
+    public String getFilm() {
+        return film;
+    }
+
+    public void setFilm(String film) {
+        this.film = film;
+    }
+
+    public String getSprache() {
+        return sprache;
+    }
+
+    public void setSprache(String sprache) {
+        this.sprache = sprache;
+    }
+
+    public String getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(String dimension) {
+        this.dimension = dimension;
+    }
+
+    public String getSaal() {
+        return saal;
+    }
+
+    public void setSaal(String saal) {
+        this.saal = saal;
+    }
+}
