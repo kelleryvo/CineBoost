40fceb01e3c16c4d1ddd18b0129dfd97b3c4a3b6
Marc Vollenweider <mtv.vollenweider@gmail.com>
2017-06-26 14:34:26 +0200

    Alle Controller erstellt und mit FXML Dateien verknüpft

---
+++ src/sample/controller/LoginCo.java
@@ -0,0 +1,18 @@
+package sample.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * Created by marcvollenweider on 26.06.17.
+ */
+public class LoginCo {
+    @FXML
+    //Alle Components
+    public Button submitButton;
+
+    @FXML
+    public void initialize(){
+
+    }
+}
